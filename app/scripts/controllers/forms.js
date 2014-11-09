cd85d67cab85ad29c4b46095f545ddc9b5a7a251
unknown <saurajitb@gmail.com>
2014-11-10 02:14:41 +0530

    Added simple form validation

--- app/scripts/controllers/forms.js
+++ app/scripts/controllers/forms.js
@@ -9,9 +9,5 @@
  */
 angular.module('angularFormsApp')
   .controller('FormsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.profileDetails = {};
   });
