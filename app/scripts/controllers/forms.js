f8e30543a3f66922e81069aa34c767429cde0045
saurajit <saurajitb@gmail.com>
2014-11-11 01:41:22 +0530

    Added Custom validators

--- app/scripts/controllers/forms.js
+++ app/scripts/controllers/forms.js
@@ -9,9 +9,16 @@
  */
 angular.module('angularFormsApp')
 	.controller('FormsCtrl', function ($scope) {
+	var PAN_FORMAT_REGEX = /^[A-Z]{5}[0-9]{4}[A-Z]$/; //5 alphabets then 4 numbers then another alphabet
+	
 	$scope.example1 = {};
 	
 	$scope.submitExample2 = function() {
 		console.log($scope.builtInValidationForm);
+	};
+	
+	$scope.validateExample3 = function() {
+		$scope.customValidationForm.pan.$setValidity('panformat', PAN_FORMAT_REGEX.test($scope.customValidationForm.pan.$modelValue));
+		console.log($scope.customValidationForm);
 	};
 	});
