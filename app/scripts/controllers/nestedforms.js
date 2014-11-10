a9547ecddc93c0ac875bc5e8147d9ce3dfd4ee0c
saurajit <saurajitb@gmail.com>
2014-11-11 02:53:39 +0530

    Added nested forms

--- app/scripts/controllers/nestedforms.js
+++ app/scripts/controllers/nestedforms.js
@@ -8,10 +8,12 @@
  * Controller of the angularFormsApp
  */
 angular.module('angularFormsApp')
-  .controller('NestedformsCtrl', function ($scope) {
-    $scope.awesomeThings = [
+	.controller('NestedformsCtrl', function ($scope) {
+		$scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+		$scope.person = {};
+	});
